aa84aa5d37ccdc2b9f625b461ae77b881d796e3e
이상원 <sangwon020214@gmail.com>
2026-01-17 22:34:13 +0900

    Initial commit: HighVoca backend setup

---
+++ src/test/java/com/highvoca/HighVocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highvoca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighVocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
